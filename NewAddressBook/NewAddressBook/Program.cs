b0c48a2eb9d1b8f0baaefba86475f072cb2cfbf2
Sampada <sampadahakke102@gmail.com>
2022-02-16 17:28:38 +0530

    [Sampada] Creating New Contacts

---
+++ NewAddressBook/NewAddressBook/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewAddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===================Welcome To Address Book===================");
+            AddressBook address = new AddressBook();
+            address.CreateContact();
+            address.Display();
+            Console.ReadKey();
+        }
+    }
+}
